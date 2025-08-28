aa94c0b506ad5e32c34ec5858a75c718ed3bf9b4
larimaciel05 <145065455+larimaciel05@users.noreply.github.com>
2025-08-28 17:15:43 -0300

    .

---
+++ script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+ enunciado:1."Qual é o principal objetivo da inteligência artificial (IA)?"
+ alternativas: [
+ "Desenvolver algoritmos que imitam o comportamento humano de forma ineficiente.",
+"Automatizar tarefas repetitivas e tomar decisões baseadas em dados."
+ ]
+  },
+  {
+    enunciado:"Qual e o nome da tecnica de IA  que permite que as maquinas “aprendam” a partir de dados sem serem explicitamente programadas?"
+    alternativas: [
+"Redes neurais convolucionais",
+"Aprendizado de maquina"
+    ]
+  },
+  {
+    enunciado:
+    alternativas: [
